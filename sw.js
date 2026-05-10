2fd975fa271fc53cd626675c8a3a52445538472a
flafstar <aimariia66@gmail.com>
2026-05-11 00:26:40 +0900

    update icon to oni mask, proper PWA sizes (180 + 512)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'zen-gym-v3';
-const FILES = ['./', './index.html', './icon.png', './manifest.json'];
+const CACHE = 'zen-gym-v4';
+const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
