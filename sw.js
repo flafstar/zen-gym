67980e8a44a839982fc8121005e73914168e1aa1
flafstar <aimariia66@gmail.com>
2026-05-11 23:21:39 +0900

    Life Log scroll: explicit 100dvh-based height instead of flex:1 (iOS reliability)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v19';
+const CACHE = 'zen-gym-v20';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
