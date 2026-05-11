fc2892077566768245286d169d49f1fb1e10d6f2
flafstar <aimariia66@gmail.com>
2026-05-11 22:56:14 +0900

    Fix duration timer flow: phase ordered correctly, button says 'I'm ready', countdown shows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v14';
+const CACHE = 'zen-gym-v15';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
