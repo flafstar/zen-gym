c73860f5c3d801b5c32eac0b504dd3c71a770db7
flafstar <aimariia66@gmail.com>
2026-05-11 22:11:40 +0900

    Life Log: tighter layout (3 cards instead of 6, /10 scales fit one row)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v5';
+const CACHE = 'zen-gym-v6';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
