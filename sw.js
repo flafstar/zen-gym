0d40e3f2a291ca3a1cca4bdb61a66b9661096e3c
flafstar <aimariia66@gmail.com>
2026-05-28 08:44:50 +0900

    Add Krasavin locked picks: morning energy + evening night gym

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v27';
+const CACHE = 'zen-gym-v28';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
