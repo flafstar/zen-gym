936af9bd14d6beb5f21784ce670334ced63187ab
flafstar <aimariia66@gmail.com>
2026-07-20 02:28:41 +0900

    Fix: swap Hanging Leg Raise for Cable Pallof Press in Gym Protocol
    Hanging Leg Raise is on the banned list for CPPS/pelvic-floor tension
    (loaded spinal flexion) - shouldn't have been added. Cable Pallof Press
    covers the same weekend core slot without spinal loading.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v35';
+const CACHE = 'zen-gym-v36';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
