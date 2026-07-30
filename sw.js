ddedc7d1260dc7653e226ad576d4d1773860152a
flafstar <aimariia66@gmail.com>
2026-07-30 20:23:11 +0900

    Rework home push/pull exercises for V-taper/aesthetic goal, highlight today's default gym day

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v38';
+const CACHE = 'zen-gym-v39';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
