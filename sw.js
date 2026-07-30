a9cfbaec54add15b9f3f513c15c9344f29093e38
flafstar <aimariia66@gmail.com>
2026-07-30 20:39:09 +0900

    Reorder Full Session exercises for better back/chest antagonist alternation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v40';
+const CACHE = 'zen-gym-v41';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
