aee6e51fc4990be7a052deb0d538b2b8d0aba678
flafstar <aimariia66@gmail.com>
2026-09-09 23:38:37 +0900

    Polish mobile workout dashboard hierarchy and readability

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zen-gym-v48';
+const CACHE = 'zen-gym-v49';
 const FILES = ['./', './index.html', './icon.png', './icon-180.png', './manifest.json'];
 
 self.addEventListener('install', e => {
